2bf1a567bae4b83df6b4afa60f15513bc22170ac
madhulika1107 <madhulikasrivastava11@gmail.com>
2020-04-10 17:25:09 -0700

    Added git class for got testing

---
+++ src/test/java/com/git/test/Git.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+import org.junit.Test;
+
+public class Git {
+	
+	@Test
+	public void gitTest() {
+		
+		System.out.println("This is just checking the if git is working or not");
+	}
+
+}
